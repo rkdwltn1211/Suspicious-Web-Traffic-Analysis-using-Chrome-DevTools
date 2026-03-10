70779ca6162d827dcae63cb268fb31193970ecdf
rkdwltn1211 <rkdwltn1211@hs.ac.kr>
2026-03-10 18:24:35 +0900

    add

---
+++ extension/vite.config.js
@@ -0,0 +1,15 @@
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: "src/background.js",
+      output: {
+        entryFileNames: "background.js",
+        format: "es"
+      }
+    }
+  }
+});
